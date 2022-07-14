cd692a0ae96a1d9166a3dec4cfd83befa55cf600
Toby Dixon <thetjdixon1234@gmail.com>
2022-07-14 02:00:33 +0100

    changed router to hashrouter and built the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import LandingPage from './Pages/LandingPage'
 import CreatePost from './Pages/CreatePost'
-import { Route, Routes, BrowserRouter, Navigate} from 'react-router-dom' 
+import { Route, Routes, HashRouter, Navigate} from 'react-router-dom' 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<LandingPage />} />
           <Route path='/create_post' element={<CreatePost />} />
           
           {/* Redirect if url doesn't exist */}
-          <Route path='*' element={<Navigate to='/' />} />
+          <Route path='*' element={<Navigate to='/' replace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
